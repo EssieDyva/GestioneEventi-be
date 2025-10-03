9c66c4e5e2d599be226b88b573b59b8621647a50
EssieDyva <st3f1703@gmail.com>
2025-10-03 12:41:00 +0200

    endpoint for all events associated with the user

--- src/main/java/com/gestioneEventi/repositories/EventRepository.java
+++ src/main/java/com/gestioneEventi/repositories/EventRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -19,4 +20,12 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @EntityGraph(attributePaths = { "invitedGroups", "invitedGroups.members", "createdBy", "createdBy.role" })
     @Override
     Optional<Event> findById(Long id);
+
+    @Query("""
+            SELECT e FROM event e
+            INNER JOIN event_groups eg ON e.id = eg.event_id
+            INNER JOIN users u ON eg.group_id = u.group_id
+            WHERE u.id = :id
+            """)
+    List<Event> findAllByUserId(Long id);
 }
